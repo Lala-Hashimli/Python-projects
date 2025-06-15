6a4ae2426dab6b29e3517409ada30fc5e5f7d8c1
Lala Hashimli <hshml.lala@gmail.com>
2025-06-16 01:56:51 +0400

    Add files via upload

---
+++ drink_stoke.py
@@ -0,0 +1,28 @@
+drink_stock = {
+    "Coca-cola 0.3L" : {"amount": 1.70},
+    "Coca-cola 0.5L" : {"amount": 2.60},
+    "Coca-cola 0.8L" : {"amount": 3.10},
+    
+    "Coca-cola Zero 0.3L" : {"amount": 1.70},
+    "Coca-cola Zero 0.5L" : {"amount": 2.60},
+    "Coca-cola Zero 0.8L" : {"amount": 3.10},
+
+    "Fanta 0.3L" : {"amount": 1.70},
+    "Fanta 0.5L" : {"amount": 2.50},
+    "Fanta 0.8L" : {"amount": 3.10},
+
+    "Sprite 0.3L" : {"amount": 1.70},
+    "Sprite 0.5L" : {"amount": 2.60},
+    "Sprite 0.8L" : {"amount": 3.10},
+    
+    "Fuse tea mango-pineapple" : {"amount": 2.50},
+    "Fuse tea lemon" : {"amount": 2.50},
+    "Fuse tea peach" : {"amount": 2.50},
+    
+    "Carry Sun cherry 0.2L" : {"amount": 1.70},
+    "Carry Sun multivitamin 0.2L" : {"amount": 1.70},
+    
+    "Sparkling water 0.5L" : {"amount": 1.20},
+    "Still water 0.5L" : {"amount": 1.20},
+    
+}
---
+++ food_stock.py
@@ -0,0 +1,36 @@
+from drink_stoke import drink_stock
+food_stock = {
+    "Burger" : {
+        "Big sanders burger" : {"amount": 9.20},
+        "Shefburger" : {"amount": 9.20},
+        "Cheeseburger" : {"amount": 4.00},
+        "Tauer burger" : {"amount": 7.40},
+        "Sanders burger" : {"amount": 7.40},
+        "Zinger burger" : {"amount": 7.40},
+    },
+    "Wraps" : {
+        "Boxmaster" : {"amount": 6.90},
+        "Twister" : {"amount": 5.20},
+        "Mega Roll" : {"amount": 7.90}
+    },
+    "Chicken" : {
+        "Drumsticks 1 pcs" : {"amount": 3.20},
+        "Drumsticks 2 pcs" : {"amount": 5.70},
+        "Drumsticks 3 pcs" : {"amount": 7.90},
+        "Wing 3 pcs" : {"amount": 4.10},
+        "Wing 6 pcs" : {"amount": 7.10},
+        "Wing 9 pcs" : {"amount": 9.80},
+        "Strips 3 pcs" : {"amount": 4.20},
+        "Strips 6 pcs" : {"amount": 7.20},
+        "Strips 9 pcs" : {"amount": 9.80},
+    },
+    "Sauces" : {
+        "Tomato ketchup" : {"amount": 0.70},
+        "BBQ sauce" : {"amount": 0.70},
+        "Garlic sauce" : {"amount": 0.70},
+        "Cheese sauce" : {"amount": 0.70},
+        "Sweet-sour sauce" : {"amount": 0.70}
+    },
+    "Cold Drinks": drink_stock,
+    
+}
